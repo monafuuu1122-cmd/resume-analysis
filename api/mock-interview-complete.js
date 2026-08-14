691f86b3ade8e7b689e7c60ce0bdf2ab9c9d8ad8
monafuuu1122-cmd <monafuuu1122@gmail.com>
2026-08-14 13:53:05 +0800

    Delete api directory

--- api/mock-interview-complete.js
+++
@@ -1,5 +0,0 @@
-import { handleWorkerRequest } from './_worker-handler.js'
-
-export default function handler(req, res) {
-  return handleWorkerRequest(req, res, `/api/mock-interview/${req.query.sessionId}/complete`)
-}
--- api/service-status.js
+++
@@ -1,7 +0,0 @@
-export default function handler(_req, res) {
-  res.statusCode = 200
-  res.setHeader('content-type', 'application/json; charset=utf-8')
-  res.end(JSON.stringify({
-    researchConfigured: Boolean(process.env.DEEPSEEK_API_KEY),
-  }))
-}
