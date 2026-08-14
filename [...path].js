4b64b55f08f789d497c2355f0390401b8b90da70
monafuuu1122-cmd <monafuuu1122@gmail.com>
2026-08-14 11:01:35 +0800

    Delete [...path].js

--- [...path].js
+++
@@ -1,6 +0,0 @@
-import { handleWorkerRequest } from './_worker-handler.js'
-
-export default function handler(req, res) {
-  const pathname = String(req.url ?? '/api').split('?')[0] || '/api'
-  return handleWorkerRequest(req, res, pathname)
-}
